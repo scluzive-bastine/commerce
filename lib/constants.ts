b6b00aa0c5c849e456692f7691d4b77743dfaa95
Sabastine <toshfuture@gmail.com>
2023-09-02 03:09:33 +0100

    image on homepage changed

--- lib/constants.ts
+++ lib/constants.ts
@@ -32,6 +32,6 @@ export const SHOPIFY_GRAPHQL_API_ENDPOINT = '/api/2023-01/graphql.json';
 export const hmbg =
   'https://images.unsplash.com/photo-1578768079052-aa76e52ff62e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3634&q=80';
 export const hmbg2 =
-  'https://img.freepik.com/free-photo/woman-model-business-suit-wearing-hat_1303-17693.jpg?w=2000&t=st=1693273897~exp=1693274497~hmac=c2a41f598d9de80cb7ae2e8f8fb83e0ba1fb81cb9d63fba3309d93501bfc3b55';
+  'https://images.unsplash.com/photo-1568251723346-462c2abfd420?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2575&q=80';
 export const twbg =
   'https://images.unsplash.com/photo-1572986339313-6fb01aa14717?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3687&q=80';
